53d193c3e44779e49471659bf1f46a3a9730eca8
Illia Shulha <ilyxa2836@gmail.com>
2023-04-08 21:02:50 +0200

    task 1 completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,26 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const galleryItemsEl = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+  )
+  .join('');
+
+galleryEl.innerHTML = galleryItemsEl;
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
